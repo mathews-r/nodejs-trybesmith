6bc51dc7e6fdcfa7e8f3b08fc21870ad68a5b8f9
Mathews <mathewsrodrigues9@gmail.com>
2022-11-05 10:10:15 -0300

    requisito 6 concluido

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -1,10 +1,16 @@
 import express from 'express';
 import ProductController from '../controllers/product.controller';
+import { amountValidation, nameValidation } from '../middlewares/productValidation';
 
 const productController = new ProductController();
 const productRouter = express.Router();
 
-productRouter.post('/', (req, res) => productController.create(req, res));
+productRouter.post(
+  '/', 
+  nameValidation, 
+  amountValidation,
+  (req, res) => productController.create(req, res),
+);
 
 productRouter.get('/', (req, res) => productController.findAll(req, res));
 
