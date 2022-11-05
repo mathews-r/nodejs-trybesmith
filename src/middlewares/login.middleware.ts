46fb166a275df5d1edb1b754aab622951e1bb8e3
Mathews <mathewsrodrigues9@gmail.com>
2022-11-05 11:55:49 -0300

    requisito 7 concluido

--- src/middlewares/login.middleware.ts
+++ src/middlewares/login.middleware.ts
@@ -6,6 +6,15 @@ export function usernameValidation(req: Request, res: Response, next: NextFuncti
     return res.status(400).json({ message: '"username" is required' });
   }
 
+  if (typeof username !== 'string') {
+    return res.status(422).json({ message: '"username" must be a string' });
+  }
+
+  if (typeof username === 'string' && username.length < 3) {
+    return res.status(422)
+      .json({ message: '"username" length must be at least 3 characters long' });
+  }
+
   next();
 }
 
@@ -15,5 +24,49 @@ export function passwordValidation(req: Request, res: Response, next: NextFuncti
     return res.status(400).json({ message: '"password" is required' });
   }
 
+  if (typeof password !== 'string') {
+    return res.status(422).json({ message: '"password" must be a string' });
+  }
+
+  if (typeof password === 'string' && password.length < 8) {
+    return res.status(422)
+      .json({ message: '"password" length must be at least 8 characters long' });
+  }
+  next();
+}
+
+export function classValidation(req: Request, res: Response, next: NextFunction) {
+  const { classe } = req.body;
+
+  if (!classe) {
+    return res.status(400).json({ message: '"classe" is required' });
+  }
+
+  if (typeof classe !== 'string') {
+    return res.status(422).json({ message: '"classe" must be a string' });
+  }
+
+  if (typeof classe === 'string' && classe.length < 3) {
+    return res.status(422)
+      .json({ message: '"classe" length must be at least 3 characters long' });
+  }
+  next();
+}
+
+export function levelValidation(req: Request, res: Response, next: NextFunction) {
+  const { level } = req.body;
+  
+  if (level === undefined) {
+    return res.status(400).json({ message: '"level" is required' });
+  }
+
+  if (typeof level !== 'number') {
+    return res.status(422).json({ message: '"level" must be a number' });
+  }
+  
+  if (typeof level === 'number' && level <= 0) {
+    return res.status(422)
+      .json({ message: '"level" must be greater than or equal to 1' });
+  }
   next();
 }
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,9 +1,18 @@
 import express from 'express';
 import UserController from '../controllers/user.controller';
+import { classValidation, levelValidation, passwordValidation, usernameValidation }
+  from '../middlewares/login.middleware';
 
 const userController = new UserController();
 const userRouter = express.Router();
 
-userRouter.post('/', (req, res) => userController.create(req, res));
+userRouter.post(
+  '/',
+  usernameValidation,
+  passwordValidation,
+  classValidation,
+  levelValidation,
+  (req, res) => userController.create(req, res),
+);
 
 export default userRouter;
